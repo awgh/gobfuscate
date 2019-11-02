c4493fcb57403796d13e5307e42d3dc494000e9c
Aw Gh <awgh@awgh.org>
2019-11-02 15:03:41 -0700

    Finished moving main() to argparse library.

--- gobfuscate/main.go
+++ gobfuscate/main.go
@@ -4,11 +4,11 @@ import (
 	"crypto/rand"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/akamensky/argparse"
 	"github.com/awgh/gobfuscate"
-	"github.com/awgh/sshell"
 )
 
 func main() {
@@ -17,15 +17,14 @@ func main() {
 	outputGopath := parser.Flag("a", "outdir", &argparse.Options{Required: false, Help: "output a full GOPATH"})
 	keepTests := parser.Flag("k", "keeptests", &argparse.Options{Required: false, Help: "keep _test.go files"})
 	winHide := parser.Flag("w", "winhide", &argparse.Options{Required: false, Help: "hide windows GUI"})
-	noStaticLink := parser.Flag("n", "nostatic", &argparse.Options{Required: false, Help: "do not statically link")
-	preservePackageName := parser.Flag("p", "noencrypt", &argparse.Options{Required: false, 
+	noStaticLink := parser.Flag("n", "nostatic", &argparse.Options{Required: false, Help: "do not statically link"})
+	preservePackageName := parser.Flag("p", "noencrypt", &argparse.Options{Required: false,
 		Help: "no encrypted package name for go build command (works when main package has CGO code)"})
-		verbose := parser.Flag("v", "verbose", &argparse.Options{Required: false, Help: "verbose mode")
-	
-	args = append([]string{"gobfuscate"}[:], args...)
-	if err := parser.Parse(args); err != nil || *srcFile == "" {
-		sshell.WriteTerm(term, parser.Usage(err))
-		return err
+	verbose := parser.Flag("v", "verbose", &argparse.Options{Required: false, Help: "verbose mode"})
+
+	if err := parser.Parse(os.Args); err != nil {
+		log.Println(parser.Usage(err))
+		return
 	}
 
 	if len(flag.Args()) != 2 {
@@ -37,13 +36,15 @@ func main() {
 	pkgName := flag.Args()[0]
 	outPath := flag.Args()[1]
 
-	if encKey == "" {
+	if *encKey == "" {
 		buf := make([]byte, 32)
 		rand.Read(buf)
-		encKey = string(buf)
+		*encKey = string(buf)
 	}
 
-	if !gobfuscate.Obfuscate(pkgName, outPath) {
+	if !gobfuscate.Obfuscate(gobfuscate.Config{PkgName: pkgName, OutPath: outPath,
+		EncKey: *encKey, OutputGopath: *outputGopath, KeepTests: *keepTests, WinHide: *winHide,
+		NoStaticLink: *noStaticLink, PreservePackageName: *preservePackageName, Verbose: *verbose}) {
 		os.Exit(1)
 	}
 }
